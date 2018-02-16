2d9226837f4ad2a9127d7416bd3ca7fcd051c672
howdy39 <tatsuya.nakano@topgate.co.jp>
2018-02-16 18:18:34 +0900

    Datepickerの追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDatepickerModule, MatFormFieldModule, MatInputModule, MatNativeDateModule } from '@angular/material';
 
 import { AppRoutingModule } from './app-routing-module';
 
 import { AppComponent } from './app.component';
 import { DatepickerValidationComponent } from './components/datepicker-validation/datepicker-validation.component';
-
 
 @NgModule({
   declarations: [
@@ -14,6 +15,11 @@ import { DatepickerValidationComponent } from './components/datepicker-validatio
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatInputModule,
     AppRoutingModule
   ],
   providers: [],
