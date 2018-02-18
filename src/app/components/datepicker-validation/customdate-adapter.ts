d4d4a038ee805aff661142720016db4d236026a9
howdy39 <tatsuya.nakano@topgate.co.jp>
2018-02-18 16:14:40 +0900

    日付のチェック処理を実装

--- src/app/components/datepicker-validation/customdate-adapter.ts
+++ src/app/components/datepicker-validation/customdate-adapter.ts
@@ -1,12 +1,26 @@
-// Custom DateAdapter
 import {NgModule} from '@angular/core';
 import {MatDatepickerModule, MatNativeDateModule, NativeDateAdapter, DateAdapter} from '@angular/material';
 
-// extend NativeDateAdapter's format method to specify the date format.
 export class CustomDateAdapter extends NativeDateAdapter {
-  isValid(date: Date) {
-    console.log(date);
-    return !isNaN(date.getTime());
+  parse(value: any): Date | null {
+    // 日付のフォーマットかどうかを確認
+    const matches = value.match(/^(\d{1,2})\/(\d{1,2})\/(\d{4})$/);
+    if (matches === null) {
+      return null;
+    }
+    const month = matches[1] - 1;
+    const day = matches[2];
+    const year = matches[3];
+    const date = new Date(year, month, day);
+
+    // 日付として正しくない場合のズレを確認
+    // 例）`2/31/2018` だと new Date() で `3/3/2018` になり、Month がずれるので不正
+    if (date.getFullYear() !== Number(year)
+      || date.getMonth() !== Number(month)
+      || date.getDate() !== Number(day)) {
+      return null;
+    }
+    return date;
   }
 }
 
